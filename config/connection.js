3d17f5835409349ce9d334918ea446a59292a207
kcalderon87 <kcalderon87@gmail.com>
2017-02-06 10:51:41 -0500

    updated connection

--- config/connection.js
+++ config/connection.js
@@ -1,19 +1,16 @@
-var mysql = require("mysql");
+// Dependencies
+var Sequelize = require("sequelize");
 
-var connection = mysql.createConnection({
+// Creates mySQL connection using Sequelize
+var sequelize = new Sequelize("burgers_db", "root", "", {
   host: "localhost",
-  user: "root",
-  password: "",
-  database: "burgers_db"
-});
-
-
-connection.connect(function(err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
+  dialect: "mysql",
+  pool: {
+    max: 5,
+    min: 0,
+    idle: 10000
   }
-  console.log("connected as id " + connection.threadId);
 });
 
-module.exports = connection;
+// Exports the connection for other files to use
+module.exports = sequelize;
