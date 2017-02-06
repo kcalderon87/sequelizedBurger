f7e8807e7f25a0fab9d67f6bb06c3b789472cc6f
kcalderon87 <kcalderon87@gmail.com>
2017-02-06 12:00:19 -0500

    routes

---
+++ routes/burgers_controller.js
@@ -0,0 +1,43 @@
+var db = require("../models/burger.js")
+
+module.exports = function(app) {
+
+  // GET route for getting all of the burgers
+  app.get("/api/burger", function(req, res) {
+    
+      db.Burger.findAll({})
+      .then(function(result) {
+        res.json(dbBurger);
+      });
+    })
+
+  };
+
+  // POST route for saving a new burger.
+  app.post("/api/burger", function(req, res) {
+
+    db.Burger.create({
+      burger_name: req.body.burger_name,
+      devoured: req.body.devoured,
+    }).then(function(dbBurger) {
+      res.json(dbBurger);
+    });
+
+  });
+
+  //PUT/UPDATE route
+  app.put("/api/burger", function(req, res) {
+
+    db.Burger.update({
+      burger_name: req.body.burger_name,
+      devoured: req.body.devoured
+    }, {
+      where: {
+        id: req.body.id
+      }
+    }).then(function(dbBurger) {
+      res.json(dbBurger);
+    });
+  });
+  
+};
